2c683e633917124bc501f45e0c93a705cbbcf7c3
LXJfromScratch <6.xijiang@gmail.com>
2024-08-13 21:53:57 +0800

    update

--- hypr/scripts/sleep.sh
+++ hypr/scripts/sleep.sh
@@ -1,5 +1,5 @@
 swayidle -w timeout 300 'swaylock -f' \
             timeout 360 'hyprctl dispatch dpms off' \
             resume 'hyprctl dispatch dpms on'\
-            timeout 3000 'systemctl suspend' \
+            timeout 30000 'systemctl suspend' \
             before-sleep 'swaylock -f' &
